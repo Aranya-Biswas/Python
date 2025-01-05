ad70679fe8342b1b3bbeb5ec305d63accc099f89
ARANYA BISWAS <aranyabiswas123@gmail.com>
2025-01-05 11:56:08 +0530

    sum_of_digit

---
+++ square_of_odd_numbers.py
@@ -0,0 +1,9 @@
+n=int(input("enter the range : "))
+num=1
+while num<n:
+    if num%2!=0:
+        print("The odd number : ",num)
+        print("The square of odd number : ",num**2)
+
+    num+=2    
+
